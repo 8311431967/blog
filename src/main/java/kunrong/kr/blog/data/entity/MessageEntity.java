9541635beae6febda04d44517d07dbf704a91c44
chrom <614005313@qq.com>
2018-06-22 23:59:51 +0800

    message

---
+++ src/main/java/kunrong/kr/blog/data/entity/MessageEntity.java
@@ -0,0 +1,93 @@
+package kunrong.kr.blog.data.entity;
+
+import org.hibernate.validator.internal.engine.messageinterpolation.parser.MessageState;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+@Entity
+@Table(name = "message")
+public class MessageEntity {
+    private int id;
+    private String content;
+    private String url;
+    private MessageState isRead;
+    private Timestamp createdAt;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "content")
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Basic
+    @Column(name = "url")
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Basic
+    @Enumerated(EnumType.STRING)
+    @Column(name = "isRead")
+    public MessageState getIsRead() {
+        return isRead;
+    }
+
+    public void setIsRead(MessageState isRead) {
+        this.isRead = isRead;
+    }
+
+    @Basic
+    @Column(name = "created_at")
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MessageEntity that = (MessageEntity) o;
+
+        if (id != that.id) return false;
+        if (content != null ? !content.equals(that.content) : that.content != null) return false;
+        if (url != null ? !url.equals(that.url) : that.url != null) return false;
+        if (isRead != null ? !isRead.equals(that.isRead) : that.isRead != null) return false;
+        if (createdAt != null ? !createdAt.equals(that.createdAt) : that.createdAt != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (content != null ? content.hashCode() : 0);
+        result = 31 * result + (url != null ? url.hashCode() : 0);
+        result = 31 * result + (isRead != null ? isRead.hashCode() : 0);
+        result = 31 * result + (createdAt != null ? createdAt.hashCode() : 0);
+        return result;
+    }
+}
